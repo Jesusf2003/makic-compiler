1b89644e0abbe611c0ef5871c635ecdfa1925ed0
Jesusf2003 <fernando.canales@vallegrande.edu.pe>
2025-08-23 22:58:47 -0500

    Simple copy file function

--- lib/cmdlib.cpp
+++ lib/cmdlib.cpp
@@ -1,13 +1,27 @@
 #include "cmdlib.h"
+#include <iostream>
 
-void Msg_Err(const char *err, ...) {
+extern int myargc;
+extern char **myargv;
+
+void Cmd_Error(const char *err, ...) {
     va_list args;                           // Lista de argumentos
     va_start(args, err);                    // Empieza a leer los argumentos
 
-    std::cerr << "You have an error!!!\n";  // Cabezal
+    std::cerr << "************ ERROR ************\n";  // Cabezal
     vfprintf(stderr, err, args);            // imprimir en stderr
     va_end(args);                           // Termina de leer los argumentos
 
     std::cerr << "\n";
     exit(1);                                // salir con código de error
+}
+
+void Cmd_Copy(const std::string& from, const std::string& to)
+{
+    try {
+        std::filesystem::create_directories(std::filesystem::path(to).parent_path());
+        std::filesystem::copy_file(from, to, std::filesystem::copy_options::overwrite_existing);
+    } catch (const std::filesystem::filesystem_error& e) {
+        std::cerr << "Error at copy file" << e.what() << "\n";
+    }
 }
--- lib/cmdlib.h
+++ lib/cmdlib.h
@@ -1,15 +1,15 @@
 #ifndef CMDLIB_H
 #define CMDLIB_H
 
-#include <iostream>
 #include <cstdlib>
 #include <cstdarg>
 #include <string>
+#include <cctype>
+#include <optional>
+#include <filesystem>
 
-//
-// Validations
-//
-void Msg_Info(const char* info);
-void Msg_Err(const char* err, ...); // prints a error message
+void Cmd_Error(const char *err, ...); // prints a error message
+
+void Cmd_Copy(const std::string &from, const std::string &to);
 
 #endif
