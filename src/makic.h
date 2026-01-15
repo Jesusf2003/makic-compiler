898f29650ffd387e864e83c4f99f353ed45dcbaf
Jesusf2003 <fernando.canales@vallegrande.edu.pe>
2026-01-14 22:47:21 -0500

    adding new options to code

--- src/makic.h
+++ src/makic.h
@@ -1,11 +1,14 @@
+#ifndef MAKIC_H
+#define MAKIC_H
+
 #include <stdio.h>
 #include <string.h>
 #include <stdint.h>
+#include <stdlib.h>
 
 #define MAKIC_DEFINE_FLAGS
 #define MAKIC_TYPE_FLAGS
 
-//
 #if 1
 /* Helpers to allow for a whole lot of flags. Otherwise we'd limit
  * to 32 or 64 -f options...
@@ -18,39 +21,66 @@ typedef struct
 #define LONGBIT(bit) {((bit) / 32), ((bit) % 32)}
 #define LONGBIT_SET(B, I) ((B).idx = (I) / 32, (B).bit = ((I) % 32))
 #else
-typedef uint32_t longbit;
+typedef uint32_t LongBit;
 #define LONGBIT(bit) (bit)
 #define LONGBIT_SET(B, I) ((B) = (I))
 #endif
 
+typedef struct
+{
+    const char *name;
+    LongBit bit;
+} opts_flag_def_t;
+
+bool opts_setflag(const char *, bool);
+bool opts_setwarn(const char *, bool);
+bool opts_setwerror(const char *, bool);
+bool opts_setoptim(const char *, bool);
+
+void opts_init(const char *, int, size_t);
+void opts_set(uint32_t *, size_t, bool);
+void opts_setoptimlevel(unsigned int);
+void opts_ini_init(const char *);
+
+/* Saner flag handling */
+void opts_backup_non_Wall(void);
+void opts_restore_non_Wall(void);
+void opts_backup_non_Werror_all(void);
+void opts_restore_non_Werror_all(void);
+
 enum
 {
-    #define MAKIC_TYPE_FLAGS
-    #define MAKIC_DEFINE_FLAG(X) X,
-    #include "opts.def"
+#define MAKIC_TYPE_FLAGS
+#define MAKIC_DEFINE_FLAG(X) X,
+#include "opts.def"
     COUNT_FLAGS
 };
 enum
 {
-    #define MAKIC_TYPE_WARNS
-    #define MAKIC_DEFINE_FLAG(X) WARN_##X,
-    #include "opts.def"
+#define MAKIC_TYPE_WARNS
+#define MAKIC_DEFINE_FLAG(X) WARN_##X,
+#include "opts.def"
     COUNT_WARNINGS
 };
 enum
 {
-    #define MAKIC_TYPE_OPTIMIZATIONS
-    #define MAKIC_DEFINE_FLAG(NAME, MIN_O) OPTIM_##NAME,
-    #include "opts.def"
+#define MAKIC_TYPE_OPTIMIZATIONS
+#define MAKIC_DEFINE_FLAG(NAME, MIN_O) OPTIM_##NAME,
+#include "opts.def"
     COUNT_OPTIMIZATIONS
 };
 enum
 {
-    #define MAKIC_TYPE_OPTIONS
-    #define MAKIC_DEFINE_FLAG(X) OPTION_##X,
-    #include "opts.def"
+#define MAKIC_TYPE_OPTIONS
+#define MAKIC_DEFINE_FLAG(X) OPTION_##X,
+#include "opts.def"
     OPTION_COUNT
 };
+extern const opts_flag_def_t opts_flag_list[COUNT_FLAGS+1];
+extern const opts_flag_def_t opts_warn_list[COUNT_WARNINGS+1];
+extern const opts_flag_def_t opts_opt_list[COUNT_OPTIMIZATIONS+1];
+extern const unsigned int opts_opt_oflag[COUNT_OPTIMIZATIONS+1];
+extern unsigned int opts_optimizationcount[COUNT_OPTIMIZATIONS];
 
 // opts
 typedef struct {
@@ -81,8 +111,18 @@ typedef struct
 } opts_cmd_t;
 extern opts_cmd_t opts;
 
-typedef struct
-{
-    const char *name;
-    LongBit bit;
-} opts_flag_def_t;
+#define OPTS_GENERIC(f, i) (!!(((f)[(i) / 32]) & (1U << (unsigned)((i) % 32))))
+
+#define OPTS_FLAG(i) OPTS_GENERIC(opts.flags, (i))
+#define OPTS_WARN(i) OPTS_GENERIC(opts.warn, (i))
+#define OPTS_WERROR(i) OPTS_GENERIC(opts.werror, (i))
+#define OPTS_OPTIMIZATION(i) OPTS_GENERIC(opts.optimization, (i))
+
+#define OPTS_OPTION_DUPED(X) (opts.options[X].allocated)
+#define OPTS_OPTION_BOOL(X) (opts.options[X].data.b)
+#define OPTS_OPTION_U16(X) (opts.options[X].data.u16)
+#define OPTS_OPTION_U32(X) (opts.options[X].data.u32)
+#define OPTS_OPTION_STR(X) (opts.options[X].data.str.c)
+#define OPTS_OPTION_DUP(X) *(OPTS_OPTION_DUPED(X) = true, &(opts.options[X].data.str.p))
+
+#endif
--- src/opts.c
+++ src/opts.c
@@ -31,6 +31,39 @@ const opts_flag_def_t opts_flag_list[COUNT_FLAGS+1] =
 unsigned int opts_optimization_count[COUNT_OPTIMIZATIONS];
 opts_cmd_t opts;
 
+static void opts_setdefault(void)
+{
+    memset(&opts, 0, sizeof(opts_cmd_t));
+    OPTS_OPTION_STR(OPTION_PROGSRC) = "progs.src";
+    opts_set(opts.warn, WARN_UNUSED_VARIABLE, true);
+    OPTS_OPTION_U32(OPTION_STATE_FPS) = 10;
+}
+
+void opts_backup_non_Wall()
+{
+    size_t i;
+    for(i = 0; i <= WARN_DEBUG; ++i)
+        opts_set(opts.warn_backup, i, OPTS_WARN(i));
+}
+void opts_restore_non_Wall()
+{
+    size_t i;
+    for(i = 0; i <= WARN_DEBUG; ++i)
+        opts_set(opts.warn, i, OPTS_GENERIC(opts.warn_backup, i));
+}
+void opts_backup_non_Werror_all()
+{
+    size_t i;
+    for(i = 0; i <= WARN_DEBUG; ++i)
+        opts_set(opts.werror_backup, i, OPTS_WERROR(i));
+}
+void opts_restore_non_Werror_all()
+{
+    size_t i;
+    for (i = 0; i <= WARN_DEBUG; ++i)
+        opts_set(opts.werror, i, OPTS_GENERIC(opts.werror_backup, i));
+}
+
 static bool opts_setflag_all(const char *name, bool on, uint32_t *flags, const opts_flag_def_t *list, size_t listsize)
 {
     size_t i;
@@ -52,6 +85,54 @@ static bool opts_setflag_all(const char *name, bool on, uint32_t *flags, const o
 bool opts_setflag(const char *name, bool on)
 {
     return opts_setflag_all(name, on, opts.flags, opts_flag_list, COUNT_FLAGS);
+}
+bool opts_setwarn(const char *name, bool on)
+{
+    return opts_setflag_all(name, on, opts.warn, opts_warn_list, COUNT_WARNINGS);
+}
+bool opts_setwerror(const char *name, bool on)
+{
+    return opts_setflag_all(name, on, opts.werror, opts_warn_list, COUNT_WARNINGS);
+}
+bool opts_setoptim(const char *name, bool on)
+{
+    return opts_setflag_all(name, on, opts.optimization, opts_opt_list, COUNT_OPTIMIZATIONS);
+}
+
+void opts_set(uint32_t *flags, size_t idx, bool on)
+{
+    LongBit lb;
+    LONGBIT_SET(lb, idx);
+    if (on)
+        flags[lb.idx] |= (1u<<(lb.bit));
+    else
+        flags[lb.idx] &= ~(1u<<(lb.bit));
+}
+void opts_setoptimlevel(unsigned int level)
+{
+    size_t i;
+    for(i = 0; i < COUNT_OPTIMIZATIONS; ++i)
+        opts_set(opts.optimization, i, level >= opts_opt_oflag[i]);
+    if (!level)
+        opts.optimizeoff = true;
+}
+
+/*
+ * Standard configuration parser and subsystem.  Yes, optionally you may
+ * create ini files or cfg (the driver accepts both) for a project opposed
+ * to supplying just a progs.src (since you also may need to supply command
+ * line arguments or set the options of the compiler) [which cannot be done
+ * from a progs.src.
+ */
+
+ /*
+ * returns true/false for a char that contains ("true" or "false" or numeric 0/1)
+ */
+static bool opts_ini_bool(const char *value)
+{
+    if (!strcmp(value, "true")) return true;
+    if (!strcmp(value, "false")) return false;
+    return !!strtol(value, nullptr, 10);
 }
 
 // undef all
